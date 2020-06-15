f091d2c1e930ad5648b5f83c34115e2d3b2f5acb
César Alberca <cesalberca@gmail.com>
2020-06-15 20:59:55 +0200

    Add READMEs

--- src/workers/main-worker.ts
+++ src/workers/main-worker.ts
@@ -1,2 +1,2 @@
-const worker = new Worker("./worker.ts", { type: "module", deno: true });
+const worker = new Worker(new URL("worker.ts", import.meta.url).href, { type: "module", deno: true });
 worker.postMessage({ filename: "./log.txt" });
