5a888c05a3fabfd815406a3458e1a7a8b5bf3642
César Alberca <cesalberca@gmail.com>
2020-06-15 21:20:00 +0200

    Add version to url

--- src/testing/testing.test.ts
+++ src/testing/testing.test.ts
@@ -1,6 +1,6 @@
-import { assertEquals } from "https://deno.land/std/testing/asserts.ts";
+import { assertEquals } from "https://deno.land/std@0.57.0/testing/asserts.ts";
 
-Deno.test("should add value", () => {
+Deno.test("should add values", () => {
   const actual = 1 + 2;
 
   assertEquals(actual, 3);
