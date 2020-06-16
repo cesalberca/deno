9a7c6d7e116360a71d83f8b46b3b2f9e7db56bb6
César Alberca <cesalberca@gmail.com>
2020-06-16 20:45:31 +0200

    Flip an odd length word

--- src/kata/flipper.ts
+++ src/kata/flipper.ts
@@ -1,6 +1,7 @@
 export class Flipper {
   flip(word: string) {
-    const half = word.length / 2;
+    const isOdd = word.length % 2 !== 0
+    const half = Math.floor(word.length / 2) + (isOdd ? 1 : 0);
     const firstHalf = word.slice(0, half);
     const secondHalf = word.slice(half, word.length);
     return secondHalf + firstHalf;
